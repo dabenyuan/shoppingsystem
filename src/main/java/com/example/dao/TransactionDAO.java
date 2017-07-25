eef9c8fb2e45d228d32cd2bc4517b2f355109f56
dabenyuan <352321356@qq.com>
2017-07-25 18:27:56 +0800

    Update TransactionDAO.java

--- src/main/java/com/example/dao/TransactionDAO.java
+++ src/main/java/com/example/dao/TransactionDAO.java
@@ -5,9 +5,7 @@ import com.example.model.TransactionDO;
 import java.sql.Timestamp;
 import java.util.List;
 
-/**
- * Created by zhang on 2017/7/13.
- */
+
 public interface TransactionDAO {
     List<TransactionDO> getTransactionByUid(Integer uid);
 
